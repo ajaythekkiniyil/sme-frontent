f57666a7b17db9a03c32af52b766f0478e43659c
ajay.t <ajaythekkiniyi@gmail.com>
2025-08-03 13:44:46 +0530

    useStrapiLogin custome hooks for login, implemented single resposibility principle

--- src/app/components/login.tsx
+++ src/app/components/login.tsx
@@ -1,7 +1,5 @@
-'use client';
-
-import { useState, FormEvent } from 'react';
-import { useRouter } from 'next/navigation';
+"use client";
+import { useStrapiLogin } from "../hooks/strapiLogin"
 
 type LoginPageTemplateProps = {
     role: string,
@@ -9,47 +7,15 @@ type LoginPageTemplateProps = {
 }
 
 export default function LoginPageTemplate({ role, successUrl }: LoginPageTemplateProps) {
-    // State for the user's identifier (email or username)
-    const [identifier, setIdentifier] = useState('');
-    // State for the user's password
-    const [password, setPassword] = useState('');
-    // State for storing and displaying login errors
-    const [error, setError] = useState('');
-    // State to manage the loading status during form submission
-    const [isLoading, setIsLoading] = useState(false);
+    const { identifier,
+        password,
+        error,
+        isLoading,
+        setIdentifier,
+        setPassword,
+        handleSubmit
 
-    const router = useRouter();
-
-    const handleSubmit = async (e: FormEvent) => {
-        e.preventDefault();
-        setError('');
-        setIsLoading(true);
-
-        try {
-            const res = await fetch('/api/auth/login', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ identifier, password }),
-            });
-
-            if (res.ok) {
-                router.push(successUrl);
-
-                // Refresh the router to re-fetch server components and
-                // reflect the new authentication state (the cookie being set).
-                // This ensures protected layouts and pages update correctly.
-                router.refresh();
-            } else {
-                // const data = await res.json();
-                setError('Failed to login. Please check your credentials.');
-            }
-        } catch (err) {
-            setError('An unexpected error occurred. Please try again.');
-        } finally {
-            setIsLoading(false);
-        }
-    };
-
+    } = useStrapiLogin(successUrl)
 
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
---
+++ src/app/hooks/strapiLogin.ts
@@ -0,0 +1,47 @@
+import { useState, FormEvent } from 'react';
+import { useRouter } from 'next/navigation';
+
+export const useStrapiLogin = (successUrl: string) => {
+    // State for the user's identifier (email or username)
+    const [identifier, setIdentifier] = useState('');
+    // State for the user's password
+    const [password, setPassword] = useState('');
+    // State for storing and displaying login errors
+    const [error, setError] = useState('');
+    // State to manage the loading status during form submission
+    const [isLoading, setIsLoading] = useState(false);
+
+    const router = useRouter();
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        setError('');
+        setIsLoading(true);
+
+        try {
+            const res = await fetch('/api/auth/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ identifier, password }),
+            });
+
+            if (res.ok) {
+                router.push(successUrl);
+
+                // Refresh the router to re-fetch server components and
+                // reflect the new authentication state (the cookie being set).
+                // This ensures protected layouts and pages update correctly.
+                router.refresh();
+            } else {
+                // const data = await res.json();
+                setError('Failed to login. Please check your credentials.');
+            }
+        } catch (err) {
+            setError('An unexpected error occurred. Please try again.');
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    return { identifier, password, error, isLoading,setIdentifier, setPassword, handleSubmit }
+}
