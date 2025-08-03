4f0873fc7e98f4df70116de1a9873c30287e1d76
ajay.t <ajaythekkiniyi@gmail.com>
2025-08-03 19:06:52 +0530

    enquiry list added

--- src/app/admin/dashboard/home/enquiries/page.tsx
+++ src/app/admin/dashboard/home/enquiries/page.tsx
@@ -1,9 +1,35 @@
-import Link from "next/link";
+"use client"
+
+import { useGetEnquiries } from "@/app/hooks/getEnquiries"
 
 export default function AdminHomePage() {
+    const { enquiries } = useGetEnquiries()
+
     return (
         <div className="min-h-screen bg-gray-100 p-6">
-            Enquires list
+            <h1 className="text-2xl font-bold mb-4">Enquiries List</h1>
+            {
+                enquiries.length === 0
+                    ?
+                    (
+                        <p>No enquiries found.</p>
+                    )
+                    :
+                    (
+                        enquiries.map((item) => (
+                            <div
+                                key={item.id}
+                                className="bg-white shadow rounded p-4 mb-4 border border-gray-200"
+                            >
+                                <p className="font-semibold">Name: {item.firstName}</p>
+                                <p>Email: {item.businessEmail}</p>
+                                <p>Enquiry: {item.enquiry}</p>
+                                <p className="text-sm text-gray-500">
+                                    Submitted on: {new Date(item.createdAt).toLocaleString()}
+                                </p>
+                            </div>
+                        ))
+                    )}
         </div>
     )
 }
---
+++ src/app/hooks/getEnquiries.ts
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react"
+
+type Enquiry = {
+    id: number;
+    documentId: string;
+    firstName: string;
+    lastName: string | null;
+    company: string | null;
+    businessNumber: string | null;
+    businessEmail: string | null;
+    location: string | null;
+    field: string | null;
+    enquiry: string | null;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+};
+
+export const useGetEnquiries = () => {
+    const [enquiries, setEnquiries] = useState<Enquiry[]>([])
+
+    useEffect(() => {
+        const getEnquiry = async () => {
+            try {
+                const res = await fetch('/api/enquiries', {
+                    method: 'GET',
+                });
+
+                if (res.ok) {
+                    const data = await res.json();
+                    setEnquiries(data.data);
+                } else {
+                    console.error('Failed to fetch enquiries');
+                }
+            } catch (err) {
+                console.error('An unexpected error occurred.', err);
+            }
+        };
+
+        getEnquiry();
+    }, []);
+
+    return { enquiries }
+}
