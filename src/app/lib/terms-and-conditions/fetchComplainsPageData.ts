3940c163243aa32b073c884dc0f2446ba1dfd71a
ajay.t <ajaythekkiniyi@gmail.com>
2025-11-05 20:49:15 +0530

    changed api/complains-page

--- src/app/lib/terms-and-conditions/fetchComplainsPageData.ts
+++ src/app/lib/terms-and-conditions/fetchComplainsPageData.ts
@@ -1,7 +1,7 @@
 const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL;
 
 export async function fetchComplainsPageData() {    
-    const res = await fetch(`${STRAPI_URL}/api/terms-and-condition`);
-    if (!res.ok) throw new Error("Failed to fetch terms-and-condition");
+    const res = await fetch(`${STRAPI_URL}/api/complains-page`);
+    if (!res.ok) throw new Error("Failed to fetch complains-page");
     return res.json();
 }
